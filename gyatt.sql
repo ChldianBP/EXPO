8a0a2543c256fce611b897052c6dcb66f812fd61
ChaldianBP <abend933@gmail.com>
2023-12-06 14:41:24 +0700

    rizz

--- gyatt.sql
+++ gyatt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2023 at 08:23 AM
+-- Generation Time: Dec 06, 2023 at 08:40 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
